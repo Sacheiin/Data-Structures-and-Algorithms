7c964cba5f235829b7804f3fddf3caa6742b8b69
Sachin C <67629515+Sacheiin@users.noreply.github.com>
2023-01-19 03:30:27 +0530

    approved and updated

--- LinkedLists.py
+++ LinkedLists.py
@@ -11,10 +11,28 @@ class LinkedLists:
        self.tail = new_node
        self.length = 1
 	
+   def Print_list(self):
+	temp = self.head
+	while temp is not None:
+		print(temp.value)
+		temp = temp.next
+		
+   def append(self, value):
+	new_node = Node(value)
+	if self.length == 0:
+		self.head = new_node
+		self.tail = new_node
+	else:
+		self.tail.next = new_node
+		self.tail = new_node
+	self.length +=1
+	
 	
 my_linked_lists = LinkedLists(4)
 my_linked_lists = LinkedLists(12)
 my_linked_lists = LinkedLists(13)
+my_linked_lists.Print_list()
+my_linked_lists.append(17)
 print('head:', my_linked_lists.head.value)
 print('tail:',my_linked_lists.tail.value)
 print('Length:', my_linked_list.length)
