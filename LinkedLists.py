f9ce4d4245ca3fc01af334e0f433c53d14c6cf17
Sachin C <67629515+Sacheiin@users.noreply.github.com>
2023-01-19 02:33:55 +0530

    updated

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,16 +1,15 @@
-
 class Node:
-	def __init__(self, value):
-		self.value = value
-		self.next = None
+   def __init__(self, value):
+	self.value = value
+	self.next = None
 
 
 class LinkedLists:
    def __init__(self, value):
-      new_node = Node(value)
-      self.head = new_node
-      self.tail = new_node
-      self.length = 1
+       new_node = Node(value)
+       self.head = new_node
+       self.tail = new_node
+       self.length = 1
 	
 	
 my_linked_lists = LinkedLists(4)
