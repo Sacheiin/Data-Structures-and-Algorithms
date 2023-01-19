54255c93a742fa3d228c44902a025512e4253a2c
Sachin C <67629515+Sacheiin@users.noreply.github.com>
2023-01-19 18:10:18 +0530

    approved and updated

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,7 +1,7 @@
 class Node:
    def __init__(self, value):
-	self.value = value
-	self.next = None
+	    self.value = value
+	    self.next = None
 
 class LinkedLists:
    def __init__(self, value):
@@ -11,47 +11,47 @@ class LinkedLists:
        self.length = 1
 	
    def Print_list(self):
-	temp = self.head
-	while temp is not None:
-		print(temp.value)
-		temp = temp.next
+	    temp = self.head
+	    while temp is not None:
+		    print(temp.value)
+		    temp = temp.next
 		
    def append(self, value):
-	new_node = Node(value)
-	if self.length == 0:
-		self.head = new_node
-		self.tail = new_node
-	else:
-		self.tail.next = new_node
-		self.tail = new_node
-	self.length +=1
+	    new_node = Node(value)
+	    if self.length == 0:
+		    self.head = new_node
+		    self.tail = new_node
+	    else:
+		    self.tail.next = new_node
+		    self.tail = new_node
+	    self.length +=1
 	
    def pop(self):
-	if self.length == 0:
-		return None
-	temp = self.head
-	pre = self.head
-	while(temp.next is not None):
-		pre = temp
-		temp = temp.next
-	self.tail = pre
-	self.tail.next = None
-	self.length -= 1
-	if self.length == 0:
-		self.head = None
-		self.tail = None
-	return temp
+	    if self.length == 0:
+		    return None
+	    temp = self.head
+	    pre = self.head
+	    while(temp.next is not None):
+		    pre = temp
+		    temp = temp.next
+	    self.tail = pre
+	    self.tail.next = None
+	    self.length -= 1
+	    if self.length == 0:
+		    self.head = None
+		    self.tail = None
+	    return temp
   
    def prepend(self, value):
-	new_node = Node(value)
-	if self.length == 0:
-		self.head = new_node
-		self.tail = new_node
-	else:
-		new_node.next = self.head
-		self.head =new_node
-	self.length += 1
-	return True
+	    new_node = Node(value)
+	    if self.length == 0:
+		    self.head = new_node
+		    self.tail = new_node
+	    else:
+		    new_node.next = self.head
+		    self.head =new_node
+	    self.length += 1
+	    return True
 
 
 
@@ -65,10 +65,15 @@ my_linked_lists.Print_list()
 my_linked_lists.append(17)
 print('head:', my_linked_lists.head.value)
 print('tail:',my_linked_lists.tail.value)
-print('Length:', my_linked_list.length)
+print('Length:', my_linked_lists.length)
 #returns 2 nodes
-print(my_linked_lists.pop())
+my_linked_lists.pop()
+my_linked_lists.Print_list()
 #returns 1 node
-print(my_linked_lists.pop())
+my_linked_lists.pop()
+my_linked_lists.Print_list()
 #returns None
-print(my_linked_lists.pop())
+my_linked_lists.pop()
+my_linked_lists.Print_list()
+my_linked_lists.prepend(41)
+my_linked_lists.Print_list()
